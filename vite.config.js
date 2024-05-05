173c07343e87c54f08b962b75efc40541248e132
Yulia <mtvjulya@yahoo.com>
2024-05-05 22:36:42 +0100

    Update Vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
 import dotenv from "dotenv";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 dotenv.config();
 
 export default defineConfig({
+    base: '/Check-it/', // добавляем базовый URL
     plugins: [react()],
     define: {
         "process.env": process.env,
+    },
+    build: {
+        outDir: "build",
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+            },
+        },
     },
 });
