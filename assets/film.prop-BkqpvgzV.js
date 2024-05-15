77068a959126ef35db72107139707834d6e839f6
Yulia <mtvjulya@yahoo.com>
2024-05-16 00:48:30 +0100

    Updates

---
+++ assets/film.prop-BkqpvgzV.js
@@ -0,0 +1,1 @@
+import{P as r}from"./main-DllzQGCZ.js";const s=r.shape({posterUrlPreview:r.string.isRequired,genres:r.array.isRequired,posterUrl:r.string.isRequired}).isRequired;export{s as F};
--- assets/film.prop-CdnTFHgk.js
+++
@@ -1,1 +0,0 @@
-import{P as r}from"./main-BQT6CTlq.js";const s=r.shape({posterUrlPreview:r.string.isRequired,genres:r.array.isRequired,posterUrl:r.string.isRequired}).isRequired;export{s as F};
