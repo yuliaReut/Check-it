c0a4cdd6a18e2e92a4550bfb6817a48dc516e2b2
Yulia <mtvjulya@yahoo.com>
2024-05-15 12:55:24 +0100

    Updates

---
+++ assets/film.prop-CdnTFHgk.js
@@ -0,0 +1,1 @@
+import{P as r}from"./main-BQT6CTlq.js";const s=r.shape({posterUrlPreview:r.string.isRequired,genres:r.array.isRequired,posterUrl:r.string.isRequired}).isRequired;export{s as F};
---
+++ assets/selectors-CiXzrJxP.js
@@ -0,0 +1,1 @@
+const e=t=>t.FILMS.favouriteFilms,a=t=>t.USER.authStatus;export{e as a,a as g};
